7f9a1d91e59360ad7f288968828202592c688219
umeransari7424 <uf143137@gmail.com>
2022-11-29 19:04:44 +0500

    Somnium

---
+++ Components/Elemets/Layout/Footer.jsx
@@ -0,0 +1,80 @@
+import React from 'react'
+import {FaDiscord , FaTelegramPlane} from 'react-icons/fa'
+import {AiOutlineTwitter ,AiOutlineInstagram , AiFillYoutube} from 'react-icons/ai'
+import {GrFacebookOption} from 'react-icons/gr'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+        <div className="container-fluid px-3 py-5">
+            <div className="row">
+                <div className="col-md-3 col-12 my-3">
+                    <div>
+                        <img src="Assets/somnium.png" alt="somniumlogo" />
+                    </div> <br />
+                    <div>
+                    © 2022 by Somnium Space LTD. Somnium Space and Somnium Space Logo are trademarks of Somnium Space LTD. All other trademarks are the property of their respective owners. All rights reserved.
+                    </div>
+                </div>
+                <div className="col-md-6 col-12 text-center my-3">
+                    <div className='d-flex justify-content-between'>
+                    <div>
+                        <div>
+                            Get Somnium
+                        </div> <br />
+                        <div>
+                            Events
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            Partners
+                        </div> <br />
+                        <div>
+                            Support
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            Terms of Service
+                        </div> <br />
+                        <div>
+                            Privacy Policy
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            FAQ
+                        </div> <br />
+                        <div>
+                            Contact
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                <div className="col-md-3 col-12  text-center my-3 ">
+                    <div >
+                        <div >
+                        <FaDiscord size={"60px"} style={{padding:"6px",border:"3px solid" , borderRadius:"50%"}}/> &nbsp; &nbsp; &nbsp;
+                        <GrFacebookOption size={"60px"} style={{padding:"6px",border:"3px solid" , borderRadius:"50%"}}/>&nbsp; &nbsp; &nbsp; 
+                        <AiOutlineTwitter size={"60px"} style={{padding:"6px",border:"3px solid" , borderRadius:"50%"}}/>&nbsp; &nbsp; &nbsp; 
+                        </div> 
+                        <br />
+                        <div >
+                        <AiOutlineInstagram size={"60px"} style={{padding:"6px",border:"3px solid" , borderRadius:"50%"}}/>&nbsp; &nbsp; &nbsp; 
+                        <FaTelegramPlane size={"60px"} style={{padding:"10px",border:"3px solid" , borderRadius:"50%"}}/>&nbsp; &nbsp; &nbsp; 
+                        <AiFillYoutube size={"60px"} style={{padding:"6px",border:"3px solid" , borderRadius:"50%"}}/>&nbsp; &nbsp; &nbsp; 
+                        </div> 
+                        
+                        
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+  )
+}
+
+export default Footer
---
+++ Components/Elemets/Layout/Header.jsx
@@ -0,0 +1,104 @@
+import {AiFillWindows} from "react-icons/ai";
+const Header = () => {
+  return (
+    <div className="d-flex main-header">
+      <div className="header d-flex">
+        <nav className="navbar navbar-expand-lg ">
+          <div className="container-fluid header ">
+            <a className="navbar-brand" href="/">
+              <img src="Assets/somnium.png" alt="somniumlogo" />
+            </a>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; 
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNavDropdown"
+              aria-controls="navbarNavDropdown"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavDropdown">
+              <div>
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <a
+                    className="nav-link active blockchain"
+                    aria-current="page"
+                    href="/"
+                  >
+                    Blockchain
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="/">
+                    Mechanics
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="/">
+                    Partners
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="/">
+                    Events
+                  </a>
+                </li>
+                
+              </ul>
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <a
+                    className="nav-link active blockchain"
+                    aria-current="page"
+                    href="/"
+                  >
+                    LogIn
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="/">
+                    SignUp
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="/">
+                    Partners
+                  </a>
+                </li>
+                
+                
+              </ul>
+              </div>
+            </div>
+            &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; 
+            <div>
+              <div>
+                <button type="button" className="btn btn-primary explore">
+                  Explore Somnium App
+                </button>
+
+                <button type="button" className="btn btn-primary market">
+                  MarketPlace
+                </button>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </div>
+      <div className="download ">
+        <button type="button" className="btn btn-primary btn-client">
+          Download VR Client <AiFillWindows size={"30px"}/>{" "}
+        </button>
+
+        <button type="button" className="btn btn-primary join">
+          Join Somnium <span className="badge web">Web</span>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
